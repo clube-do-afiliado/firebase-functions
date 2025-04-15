0be20290cf3a0f7e93c5b87cc2ec5cf61b55b8cc
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-04-15 17:05:33 -0300

    feat: remove dotenv

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,6 @@
-
-import dotenv from 'dotenv';
-
 import { onRequest } from 'firebase-functions/v2/https';
 
 import { onGetInfo } from './handlers';
-
-dotenv.config();
 
 const CORS_SAFE_LIST = [
     /^(https?:\/\/)?([a-zA-Z0-9-]+\.)*clubedoafiliado\.com(\/|$)/,
