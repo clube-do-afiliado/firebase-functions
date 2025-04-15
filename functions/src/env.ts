cedd5afc410e9e349e9945f1585028d9c769e64b
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-04-15 18:23:19 -0300

    fix: change environment name

--- functions/src/env.ts
+++ functions/src/env.ts
@@ -1,6 +1,6 @@
 import { defineString } from 'firebase-functions/params';
 
-export const env = defineString('ENV', {
+export const env = defineString('env', {
     default: 'local',
     description: 'Ambiente de execução',
 });
