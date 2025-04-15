c8b5ca0eb15bc5939fe8e0fe2f29b0be22b697b7
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-04-15 18:14:25 -0300

    feat: add firebase auth to insert environments

--- functions/src/env.ts
+++ functions/src/env.ts
@@ -1,8 +1,6 @@
 import { defineString } from 'firebase-functions/params';
 
-import type { Env } from './core';
-
 export const env = defineString('ENV', {
     default: 'local',
     description: 'Ambiente de execução',
-}).value() as Env;
+});
