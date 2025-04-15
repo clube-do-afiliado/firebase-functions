9875407222e5175e90b930737fae59afc54253d4
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-04-15 17:46:03 -0300

    feat: add environment variable to use firebase config set

--- functions/src/core/createContext.ts
+++ functions/src/core/createContext.ts
@@ -1,16 +1,14 @@
+import { env } from '@/env';
+
 import { Plugin } from './Plugin';
 import type { Request } from './Request';
-import type { Context, Env } from './Context';
-
-import { defineString } from 'firebase-functions/params';
+import type { Context } from './Context';
 
 function instantiate<T, I>(token: Plugin<T, I>, request: Request, context: Context<T>): I {
     return token(request, context);
 }
 
 export default function createContext<T>(request: Request): Context<T> {
-    const env = defineString('ENV', { default: 'local' }).value() as Env;
-
     function use<I>(token: Plugin<T, I>): I {
         return instantiate(token, request, { use, env, data: {} });
     }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,15 @@
 import { onRequest } from 'firebase-functions/v2/https';
 
+import { env } from './env';
 import { onGetInfo } from './handlers';
 
 const CORS_SAFE_LIST = [
     /^(https?:\/\/)?([a-zA-Z0-9-]+\.)*clubedoafiliado\.com(\/|$)/,
 ];
+
+export const health = onRequest((_, res) => {
+    res.status(200).send(`This app is healthy - ${env}`);
+});
 
 export const getInfo = onRequest({
     memory: '2GiB',
