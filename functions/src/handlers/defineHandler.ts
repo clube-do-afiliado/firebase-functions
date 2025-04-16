f1f371ed1a82eae4a9ac5f71d6a1acda1b814f3c
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-04-15 23:12:19 -0300

    remove catch error

--- functions/src/handlers/defineHandler.ts
+++ functions/src/handlers/defineHandler.ts
@@ -4,20 +4,16 @@ import type { Request, Response, Middleware } from '@/core';
 
 export default function defineHandler<T>(handler: (req: Request, res: Response) => Middleware<T>[]) {
     return async (req: Request, res: Response) => {
-        try {
-            const context = createContext(req);
+        const context = createContext(req);
 
-            const middlewares = handler(req, res);
+        const middlewares = handler(req, res);
 
-            const response = await runMiddlewares(middlewares ?? [], {
-                request: req,
-                context,
-                next: async () => res,
-            });
+        const response = await runMiddlewares(middlewares ?? [], {
+            request: req,
+            context,
+            next: async () => res,
+        });
 
-            response.json({ data: context.data });
-        } catch (error) {
-            res.status(500).send(error);
-        }
+        response.json({ data: context.data });
     };
 }
