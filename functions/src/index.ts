6e19004782c13babdbca95b548bc950fcfb3ae61
leonardo.goncalves <leogoncalves.contato@gmail.com>
2025-06-05 12:48:56 -0300

    change hmg to stg

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,16 +6,21 @@ const CORS_SAFE_LIST = [
     /^(https?:\/\/)?([a-zA-Z0-9-]+\.)*clubedoafiliado\.com(\/|$)/,
 ];
 
+const REGION = 'southamerica-east1';
+
 export const health = onRequest(onHealth);
 
 export const track = onRequest({
     cors: CORS_SAFE_LIST,
+    region: REGION,
 }, onTrackEvent);
 
 export const redirect = onRequest({
     cors: CORS_SAFE_LIST,
+    region: REGION,
 }, onRedirect);
 
 export const shortUrl = onRequest({
     cors: CORS_SAFE_LIST,
+    region: REGION,
 }, onGenerateShortUrl);
